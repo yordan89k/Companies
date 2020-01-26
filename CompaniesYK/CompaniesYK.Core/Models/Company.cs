32e885e6be88586dda63ba9313709798d4449a38
yordan89k <54133275+yordan89k@users.noreply.github.com>
2020-01-26 13:13:31 +0100

    Created the 2 models

---
+++ CompaniesYK/CompaniesYK.Core/Models/Company.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompaniesYK.Core.Models
+{
+    public class Company : Base
+    {
+
+        [StringLength(225)]
+        [DisplayName("Company Name")]
+        public string Name { get; set; }
+        public int OrganizationNumber { get; set; }
+        public string Logo { get; set; }
+    }
+}
---
+++ CompaniesYK/CompaniesYK.Core/Models/Store.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompaniesYK.Core.Models
+{
+    public class Store : Base
+    {
+
+
+        public int CompanyId { get; set; }
+        // Should this be int or something else since it comes from the Company class?
+
+        [StringLength(100)]
+        [DisplayName("Store Name")]
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string City { get; set; }
+        public string Zip { get; set; }
+        public string Country { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+
+
+
+    }
+}
